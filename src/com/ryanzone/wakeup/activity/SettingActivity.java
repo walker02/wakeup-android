312761a933607d0ed3e38a3d9424bcfa868a8da0
walker02 <zhangfuli05@126.com>
2013-12-23 10:21:49 +0800

    deleted:    res/layout/ppw_music_choice.xml 	modified:   res/values/strings.xml 	modified:   src/com/ryanzone/wakeup/activity/SettingActivity.java

--- src/com/ryanzone/wakeup/activity/SettingActivity.java
+++ src/com/ryanzone/wakeup/activity/SettingActivity.java
@@ -15,6 +15,8 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
@@ -43,6 +45,7 @@ public class SettingActivity extends RTActivity {
     private TimeSelectAdapter minuteAdapter;
     private GvWeekAdapter     gvWeekAdapter;
     private int               currentPosition = Integer.MAX_VALUE / 2 + 1;
+    private int               currentPositionHour = Integer.MAX_VALUE / 2 + 1;
     private PopupWindow       mPopupWindow;
     private List<String>      dayStrings;
     private RelativeLayout rlSettingRoot;
@@ -63,7 +66,14 @@ public class SettingActivity extends RTActivity {
 
             @Override
             public void onClick(View arg0) {
-                showPopuWindow(arg0);
+                showPopuWindowMusic(arg0);
+            }
+        });
+        findViewById(R.id.btnShakeStrength).setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                showPopuWindowShake(arg0);
             }
         });
         
@@ -84,8 +94,8 @@ public class SettingActivity extends RTActivity {
     }
 
     @SuppressLint("NewApi")
-    protected void showPopuWindow(View arg0) {
-        View popupView = getLayoutInflater().inflate(R.layout.ppw_music_choice, null);
+    protected void showPopuWindowMusic(View arg0) {
+        View popupView = getLayoutInflater().inflate(R.layout.ppw_setting, null);
         mPopupWindow = new PopupWindow(popupView, RelativeLayout.LayoutParams.MATCH_PARENT,
             RelativeLayout.LayoutParams.WRAP_CONTENT, true);
         mPopupWindow.setTouchable(true);
@@ -106,6 +116,62 @@ public class SettingActivity extends RTActivity {
                 rlSettingRoot.setAlpha(1);
             }
         });
+        
+        ListView lvShakeStrengh = (ListView) popupView.findViewById(R.id.lvShakeStrengh);
+        List<String> shakeStrenghStrings = new ArrayList<String>();
+        shakeStrenghStrings.add(getResources().getString(R.string.shake));
+        shakeStrenghStrings.add(getResources().getString(R.string.old_music_bell));
+        shakeStrenghStrings.add(getResources().getString(R.string.valley_voice));
+        SettingAdapter settingAdapter = new SettingAdapter(SettingActivity.this, R.layout.shake_music_list_item, shakeStrenghStrings);
+        lvShakeStrengh.setAdapter(settingAdapter);
+        lvShakeStrengh.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //get the selection content
+            }
+        });
+        
+    }
+    @SuppressLint("NewApi")
+    protected void showPopuWindowShake(View arg0) {
+        View popupView = getLayoutInflater().inflate(R.layout.ppw_setting, null);
+        mPopupWindow = new PopupWindow(popupView, RelativeLayout.LayoutParams.MATCH_PARENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT, true);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.setAnimationStyle(R.style.anim_bottom_menu);
+        if (mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        } else {
+            mPopupWindow.showAtLocation(arg0, Gravity.BOTTOM, 0, 0);
+            rlSettingRoot.setAlpha((float) 0.4);
+        }
+        
+        mPopupWindow.setOnDismissListener(new OnDismissListener() {
+            
+            @Override
+            public void onDismiss() {
+                rlSettingRoot.setAlpha(1);
+            }
+        });
+        
+        
+       ListView lvShakeStrengh = (ListView) popupView.findViewById(R.id.lvShakeStrengh);
+       List<String> shakeStrenghStrings = new ArrayList<String>();
+       shakeStrenghStrings.add(getResources().getString(R.string.easy_shake));
+       shakeStrenghStrings.add(getResources().getString(R.string.middle_shake));
+       shakeStrenghStrings.add(getResources().getString(R.string.hard_shake));
+       SettingAdapter settingAdapter = new SettingAdapter(SettingActivity.this, R.layout.shake_music_list_item, shakeStrenghStrings);
+       lvShakeStrengh.setAdapter(settingAdapter);
+       lvShakeStrengh.setOnItemClickListener(new OnItemClickListener() {
+
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             //get the selection content
+           }
+       });
     }
 
     private void initData() {
@@ -126,9 +192,12 @@ public class SettingActivity extends RTActivity {
 
         gvWeek.setAdapter(gvWeekAdapter);
         lvHour.setAdapter(hourAdapter);
-        lvHour.setSelection(Integer.MAX_VALUE / 2 + 1);
+        lvHour.setSelection(Integer.MAX_VALUE / 2 + 2);
+        hourAdapter.setSelectItem(Integer.MAX_VALUE / 2 + 2);
+        
         lvMinute.setAdapter(minuteAdapter);
-        lvMinute.setSelection(Integer.MAX_VALUE / 2 + 1);
+        lvMinute.setSelection(Integer.MAX_VALUE / 2 + 26);
+        minuteAdapter.setSelectItem(Integer.MAX_VALUE / 2 + 2);
         lvMinute.setOnScrollListener(new OnScrollListener() {
 
             @Override
@@ -147,6 +216,27 @@ public class SettingActivity extends RTActivity {
                                  int totalItemCount) {
                 //set current Item
                 currentPosition = firstVisibleItem;
+            }
+        });
+        //set hour selection and change the color 
+        lvHour.setOnScrollListener(new OnScrollListener() {
+            
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    //change the selection text color by setSelectItem
+                    hourAdapter.setSelectItem(currentPositionHour + 2);
+                    hourAdapter.notifyDataSetChanged();
+                    //select the middle
+                    lvHour.setSelection(currentPositionHour + 1);
+                }
+            }
+            
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                //set current Item
+                currentPositionHour = firstVisibleItem;
             }
         });
     }
@@ -190,7 +280,42 @@ public class SettingActivity extends RTActivity {
     class GvWeekHolder {
         TextView tvDay;
     }
+    
+    class SettingAdapter extends ArrayAdapter<String> {
 
+        private LvSettingHolder   holder;
+        int resourceId;
+        LayoutInflater inflater;
+        
+        public SettingAdapter(Context context, int resourceId, List<String> objects) {
+            super(context, resourceId, objects);
+            this.resourceId = resourceId;
+            inflater = SettingActivity.this.getLayoutInflater();
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+
+            String bean = getItem(position);
+            holder = null;
+            if (convertView == null) {
+                holder = new LvSettingHolder();
+                convertView = inflater.inflate(resourceId, parent, false);
+                holder.tvSelection = (TextView) convertView.findViewById(R.id.tvSelection);
+                convertView.setTag(holder);
+            } else {
+                holder = (LvSettingHolder) convertView.getTag();
+            }
+
+            holder.tvSelection.setText(bean);
+            return convertView;
+        }
+    }
+    
+    class LvSettingHolder {
+        TextView tvSelection;
+    }
     @Override
     public void init() {
     }
